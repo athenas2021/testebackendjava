b97ee24656bcff02a7cf069aa97fd88127c20fa7
athenas2021 <lucas.kano@outlook.com>
2021-08-29 13:36:47 -0300

    Projeto iniciado, dependencias basicas adicionadas

---
+++ src/main/java/com/lucas/kano/rastreio/RastreioApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.kano.rastreio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastreioApplication.class, args);
+	}
+
+}
