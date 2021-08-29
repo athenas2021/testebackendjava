f84879f0893cadf34b002506d849550efb7f0e31
athenas2021 <lucas.kano@outlook.com>
2021-08-29 14:01:55 -0300

    Adicionando classe Historico e Classe Objeto

---
+++ src/main/java/com/lucas/kano/rastreio/domain/Historico.java
@@ -0,0 +1,69 @@
+package com.lucas.kano.rastreio.domain;
+
+public class Historico {
+	private Integer id;
+	private String descricao;
+	private Objeto objeto;
+
+	public Historico() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Historico(Integer id, String descricao, Objeto objeto) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.objeto = objeto;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Objeto getObjeto() {
+		return objeto;
+	}
+
+	public void setObjeto(Objeto objeto) {
+		this.objeto = objeto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Historico other = (Historico) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/lucas/kano/rastreio/domain/Objeto.java
@@ -0,0 +1,59 @@
+package com.lucas.kano.rastreio.domain;
+
+public class Objeto {
+	private Integer id;
+	private String codigo;
+
+	public Objeto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Objeto(Integer id, String codigo) {
+		super();
+		this.id = id;
+		this.codigo = codigo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Objeto other = (Objeto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
